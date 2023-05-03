a7bd35cd0627532ffa006585e8a14bc0d3ee0481
rtm713 <rtm713@gmail.com>
2023-05-03 01:57:50 -0500

    added current day functionallity

--- assets/logic.js
+++ assets/logic.js
@@ -1,6 +1,8 @@
 
 var inputBox = document.querySelector('#inputBox');
 var searchButton = document.querySelector('#searchButton');
+var currentDay = document.querySelector('#currentDay');
+var fiveDay = document.querySelector('#fiveDay');
 
 var WEATHERLOCATION_API_URL = "http://api.openweathermap.org/geo/1.0/direct?limit=3&q=";
 var searchContent = "";
@@ -13,8 +15,6 @@ searchButton.addEventListener('click', function() {
     searchContent = inputBox.value;
     fetchWeatherLocationResults();
 })
-
-
 
 function fetchWeatherLocationResults() {
     fetch (WEATHERLOCATION_API_URL+searchContent+API_KEY)
@@ -46,10 +46,39 @@ function fetchWeatherResults(locationData) {
     })
     .then(function (data) {
         console.log('Data :>>', data);
-
+        renderWeatherData(data)
         // renderDrinkResults(data);
     })
     .catch(function (error) {
         console.error(error);
     });
+}
+
+function renderWeatherData(data) {
+    currentDay.textContent = " ";
+
+    var curCity = document.createElement('h2');
+    curCity.textContent = searchContent;
+    currentDay.append(curCity);
+
+    // NEED DATE !!!!! 
+
+    var icon = document.createElement('img');
+    var iconCode = data.list[0].weather[0].icon;
+    var iconUrl = "https://openweathermap.org/img/wn/" + iconCode + ".png";
+    icon.setAttribute('src', iconUrl);
+    currentDay.append(icon);
+
+
+    var temp = document.createElement('p');
+    temp.textContent = "Temp: " + data.list[0].main.temp + "°F";
+    currentDay.append(temp);
+
+    var wind = document.createElement('p');
+    wind.textContent = "Wind: " + data.list[0].wind.speed + "MPH";
+    currentDay.append(wind);
+
+    var humidity = document.createElement('p');
+    humidity.textContent = "Humidity: " + data.list[0].main.humidity + "%";
+    currentDay.append(humidity);
 }
